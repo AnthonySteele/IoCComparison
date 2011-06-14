7e15032006447acaeb13b6d30328c85b1aea3779
Anthony Steele <anthony@plasticavalon.org>
2011-06-14 11:16:09 +0100

    Unity autoreg working, using the "Unity Auto Registration" addon from http://autoregistration.codeplex.com/

--- AutoregisterTests/UnityTest.cs
+++ AutoregisterTests/UnityTest.cs
@@ -1,7 +1,9 @@
 ﻿namespace IoCComparison.AutoregisterTests
 {
-    using System.Collections.Generic;
     using System.Linq;
+    using AutoregisteredClasses.Interfaces;
+    using AutoregisteredClasses.Services;
+    using Unity.AutoRegistration;
     using Microsoft.Practices.Unity;
     using NUnit.Framework;
 
@@ -14,6 +16,30 @@
         public void CanMakeBusinessProcess()
         {
             UnityContainer container = new UnityContainer();
+            container.ConfigureAutoRegistration().
+                LoadAssemblyFrom("AutoregisteredClasses.dll").
+                Include(t => true,
+                Then.Register().AsAllInterfacesOfType()).
+                ApplyAutoRegistration();
+
+            BusinessProcess bp = container.Resolve<BusinessProcess>();
+
+            Assert.IsNotNull(bp);
+        }
+
+        [Test]
+        public void CanGetAllValidators()
+        {
+            UnityContainer container = new UnityContainer();
+            container.ConfigureAutoRegistration().
+                LoadAssemblyFrom("AutoregisteredClasses.dll").
+                Include(If.Implements<IValidator>, Then.Register().As<IValidator>().WithTypeName()). 
+                ApplyAutoRegistration();
+
+            var validators = container.ResolveAll<IValidator>();
+
+            Assert.IsNotNull(validators);
+            Assert.AreEqual(3, validators.Count());
         }
     }
 }
